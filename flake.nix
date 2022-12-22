b47c8e9e286866a5ff2f88c66dafb10902f1e846
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-21 19:23:41 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, gitignore, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustAttrs = import ./rust.nix { inherit pkgs gitignore; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustAttrs.rust-shell
+
+              # common
+              watchexec
+              just
+              nixfmt
+            ];
+          };
+        };
+        packages = {
+          default = pkgs.hello;
+          rust-bin = rustAttrs.binary;
+          rust-docker = rustAttrs.docker;
+        };
+      });
+}
