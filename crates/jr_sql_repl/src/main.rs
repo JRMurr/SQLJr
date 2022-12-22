e7e4f11198fc3774404e762d42cd3883f242e59c
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-21 20:16:51 -0500

    basic repl example

--- crates/jr_sql_repl/src/main.rs
+++ crates/jr_sql_repl/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::{self, error::ReadlineError};
+
+fn main() -> eyre::Result<()> {
+    let mut rl = rustyline::Editor::<()>::new()?;
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => println!("Line: {line:?}"),
+            Err(ReadlineError::Interrupted) => {
+                // CTRL-C so just skip
+            }
+            Err(ReadlineError::Eof) => {
+                //"CTRL-D"
+                break;
+            }
+            Err(err) => {
+                println!("Error: {err:?}");
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
