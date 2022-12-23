6a5e78e8ef301dabcc36370f0972f44ef7759d83
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-12-23 01:47:27 -0500

    basic history

--- crates/sql_jr_repl/src/main.rs
+++ crates/sql_jr_repl/src/main.rs
@@ -1,14 +1,20 @@
 use rustyline::{self, error::ReadlineError};
 use sql_jr_parser::{self, ast::SqlQuery, parse::ParseError};
+
+const HISTORY_FILE: &str = "./history.txt";
+
 fn main() -> eyre::Result<()> {
     let mut rl = rustyline::Editor::<()>::new()?;
-
+    if rl.load_history(HISTORY_FILE).is_err() {
+        println!("No previous history.");
+    }
     let mut exec = sql_jr_execution::Execution::new();
 
     loop {
         let readline = rl.readline(">> ");
         match readline {
             Ok(line) => {
+                rl.add_history_entry(line.as_str());
                 let line: &str = line.as_ref();
                 let query: Result<SqlQuery, ParseError> = line.try_into();
                 if let Ok(q) = query {
@@ -28,6 +34,7 @@ fn main() -> eyre::Result<()> {
             }
         }
     }
+    rl.save_history(HISTORY_FILE)?;
 
     Ok(())
 }
